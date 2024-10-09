161b0044a801407c67d8b69eb771470b84bf8667
ceomacharia <bedanmacharia13@gmail.com>
2024-10-09 21:01:39 +0300

    code update

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 def menu_options():
-    print("add a new task")
-    print("view sll tasks")
-    print("mark task as complete")
-    print("delete a task")
-    print("save tasks to files")
-    print("load tasks from file")
-    print("exit")
+    print("1. add a new task")
+    print("2. view all tasks")
+    print("3. mark task as complete")
+    print("4. delete a task")
+    print("5. clear all tasks")
+    print("6. exit")
+
+task_list = {}
 
 while True:
     menu_options()
-    choice = input("choose an option:")
+    choice = input("choose an option (1-7):")
 
-    if choice == "exit":
+    if choice == "7": #exit
         print("exiting")
         break
-    
+    elif choice == "1":  #add new task
+        task = input("enter task name: ")
+        description = input("enter task description: ")
+
+        task_list[task] = description
+        print(f"'{task}' task added successfully")
+
+    elif choice == "2":  #view all tasks
+        if task_list:
+            print(task_list.items())
+        else:
+            print("No current tasks")
+
+    elif choice == "4": #delete a task
